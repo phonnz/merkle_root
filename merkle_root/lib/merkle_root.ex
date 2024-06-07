0d8702cadf7d51e0986db3079e73b7ad80f2ec29
phonnz <gonzalezm.alfonso@gmail.com>
2024-06-07 20:37:01 +0200

    Initial commit

---
+++ merkle_root/lib/merkle_root.ex
@@ -0,0 +1,18 @@
+defmodule MerkleRoot do
+  @moduledoc """
+  Documentation for `MerkleRoot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MerkleRoot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ merkle_root/test/merkle_root_test.exs
@@ -0,0 +1,8 @@
+defmodule MerkleRootTest do
+  use ExUnit.Case
+  doctest MerkleRoot
+
+  test "greets the world" do
+    assert MerkleRoot.hello() == :world
+  end
+end
