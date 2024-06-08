ec532add5973bcf8dc0f1fe7987064bc7b961eb8
phonnz <gonzalezm.alfonso@gmail.com>
2024-06-08 23:07:41 +0200

    Elapsed avg time: ~1476; Merkle root by using hex package

--- merkle_root/lib/merkle_root.ex
+++ merkle_root/lib/merkle_root.ex
@@ -4,15 +4,36 @@ defmodule MerkleRoot do
   """
 
   @doc """
-  Hello world.
-
+  MerkleRoot
+  Finding the Merkle root from a provided
+  hashs array,this is using merkle_tree hex package.
   ## Examples
 
-      iex> MerkleRoot.hello()
+      iex> MerkleRoot.root()
       :world
 
   """
-  def hello do
-    :world
+  @path "../input.txt"
+  def run do
+    start = Time.utc_now()
+
+    tree =
+      @path
+      |> File.stream!()
+      |> Enum.map(&clean_row/1)
+      |> MerkleTree.new()
+
+    finished = Time.utc_now()
+
+    time_to_process = Time.diff(finished, start, :millisecond)
+    IO.inspect(tree.hash_function())
+
+    IO.puts(
+      "Elpased time to merkle root using Elixir and merkle_tree implementation : #{time_to_process}"
+    )
+  end
+
+  defp clean_row(row) do
+    String.trim_trailing(row, "\n")
   end
 end
