d1ea45d59bcea069ab5fbbc586f0f1085050cdea
phonnz <gonzalezm.alfonso@gmail.com>
2024-06-17 06:50:51 +0200

    Cleaning

--- merkle_root/src/main.rs
+++ merkle_root/src/main.rs
@@ -2,7 +2,6 @@ use sha256::digest;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::Path;
 
 #[derive(Debug, Clone)]
 struct MerkleNode {
@@ -66,60 +65,14 @@ impl MerkleTree {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Define the file path
-    let file_path = "input.txt";
-
-    // Open the file
-    let file = File::open(file_path)?;
-
-    // Create a buffered reader
+    let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-
-    // Initialize an empty vector to store lines
     let mut lines: Vec<String> = Vec::new();
-
-    // Iterate over lines in the file
-    for line in reader.lines() {
-        lines.push(line?);
-    }
-
-    // Print the resulting vector (optional)
-    println!("Lines as vector of words: {:?}", lines);
-
-    // Define the file path
-    let file_path = "input.txt";
-
-    // Open the file
-    let file = File::open(file_path)?;
-
-    // Create a buffered reader
-    let reader = BufReader::new(file);
-
-    // Initialize an empty vector to store lines
-    let mut lines: Vec<String> = Vec::new();
-
-    // Iterate over lines in the file
     for line in reader.lines() {
         lines.push(line?);
     }
     let transactions: Vec<&str> = lines.iter().map(|s| s as &str).collect();
-    // Print the resulting vector (optional)
-    println!("Lines as vector of words: {:?}", lines);
 
-    /*
-    let file = File::open(Path::new("input.txt"));
-    let reader = BufReader::new(file);
-    let mut transactions: Vec<&str> = Vec::new();
-    for line in reader.lines() {
-        transactions.push(line);
-    }
-    //let transactions: String = read_to_string(Path::new("../../input.txt")).unwrap();
-    * let transactions = vec![
-        "77d519a56a3bb197bca02ed25f880a122487914556d587588e633c8368d13053",
-        "915961583d426ff5d6726ee59ff7e1ad234d8343f60c57ab023b21741fdba723",
-        "7a172559f818c9d9f750b20f9fb16ed89879df47c20e03ffeaa3026c1d297646",
-        "2163a680ddcd3b7dcfb444d1e19395e59c63781c09e29c8d4b66bfd6460ca142",
-    ];*/
     let merkle_tree = MerkleTree::new(transactions);
 
     println!("MerkleRoot Hash: {}", merkle_tree.root_hash());
